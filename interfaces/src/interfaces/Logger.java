8ba89d1fc5a151837bdec8eb5cca269f08359825
serranuraran <90554@DESKTOP-K3RAL3M>
2021-05-04 01:48:58 +0300

    4.gün

---
+++ interfaces/src/interfaces/Logger.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public interface Logger {
+
+	 void log(String message);
+}
+
+
+//interface de referans tutucu. bir sýnýf sadece bir sýnýfý extend edebilir. 
+//ama bir sýnýf birden fazla interface',i implement edebilir.
