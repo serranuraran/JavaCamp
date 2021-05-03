b476330377b159730db47b40122ba232a143e5b0
serranuraran <90554@DESKTOP-K3RAL3M>
2021-05-04 01:47:43 +0300

    3.gün

---
+++ oopIntro/src/oopIntro/Category.java
@@ -0,0 +1,33 @@
+package oopIntro;
+
+public class Category {
+
+	private int id;
+	private String name;
+
+	public Category() {
+		super();
+	}
+
+	public Category(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) { // dýþardan birþey alýnca onu parametreye ekliyoruz.
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name + " ! ";
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ oopIntro/src/oopIntro/Main.java
@@ -0,0 +1,57 @@
+package oopIntro;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Product product0 = new Product(1, "Lenovo V14", 15000, "16 GB RAM", 10);
+
+		Product product1 = new Product(); // örnek oluþturma, referans oluþturma, instance creation(alma)
+		product1.setId(2);
+		product1.setName("Lenova V15");
+		product1.setDetail("16 GB Ram");
+		product1.setDiscount(10);
+		product1.setUnitPrice(16000);
+
+		System.out.println(product1.getUnitPriceAfterDiscount());
+
+		Product product2 = new Product();
+
+		Product product3 = new Product();
+
+		Product[] products = { product0, product1, product2, product3 };
+
+		System.out.println(products.length);
+
+		for (Product product : products) {
+
+			// System.out.println(product.name);
+		}
+
+		Category category1 = new Category();
+
+		category1.setId(1);
+		category1.setName("bilgisayar");
+
+		Category category2 = new Category();
+
+		category2.setId(2);
+		category2.setName("Ev/Bahçe");
+
+		System.out.println(category1.getName());
+		System.out.println(category2.getName());
+
+		ProductManager productManager = new ProductManager();
+		productManager.addToCart(product1); // product'ýn bellek adresini göndermiþ oluyoruz.
+		productManager.addToCart(product2);
+		productManager.addToCart(product3);
+
+	}
+
+}
+
+// kümülatif bir ürün listesi oluþtu.
+
+// persistancy = kalýcý demek. db de tutmak demek 
+
+// classlar referans tiptir.
